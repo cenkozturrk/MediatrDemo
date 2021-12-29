f81aa89092d0bbde10a9c288cf0adaaffc54cb09
Cenk Öztürk <cenk.ozturk@mikro.com.tr>
2021-12-29 12:14:48 +0300

    Queries has done.

--- MediatrDemo/Med/Queries/GetProductByIdQuery.cs
+++ MediatrDemo/Med/Queries/GetProductByIdQuery.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MediatrDemo.Med.Queries
 {
-    public class GetProductByIdQuery
+    //bir istek oluşturacak ve veri tabanından herhangir bir şey alacağız.
+    public class GetProductByIdQuery: IRequest<GetProductViewModel>
     {
+        public Guid Id { get; set; }
+
     }
 }
---
+++ MediatrDemo/Med/Queries/GetProductByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediatrDemo.Med.Queries
+{
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, GetProductViewModel>
+    {
+        public Task<GetProductViewModel> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+
+            var vievModel = new GetProductViewModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Book"
+            };
+
+            return Task.FromResult(vievModel);
+
+
+            //get product from repository
+
+        }
+
+    }
+}
